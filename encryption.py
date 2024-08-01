96b286d989c5a98bd4ee3e2950698452cd15e4bb
Jay Liu <jay.s.liu@gmail.com>
2024-08-01 13:29:52 -0400

    to GnuPG with public key encryption

--- encryption.py
+++ encryption.py
@@ -2,46 +2,67 @@ import gnupg
 import hashlib
 from config_parser import GetConfigPaster
 
-KEY = GetConfigPaster('ENCRYPTION_KEY', 'key')
+GNUPGHOME = GetConfigPaster('GPG', 'gnupg_home')
+KEYRING = GetConfigPaster('GPG', 'keyring')
+RECIPIENTS = GetConfigPaster('GPG', 'recipients')
 
 class GPGCipher(object):
-    def __init__(self, gnupghome, keyring): 
+    def __init__(self, gnupghome=None, keyring=None, recipients=None): 
         self.gnupghome = gnupghome
         self.keyring = keyring
-        if gnupghome:
-            return gnupg.GPG(gnupghome=gnupghome, keyring=keyring)
+        self.recipients = recipients
+
+    def encrypt(self, data):
+        if self.gnupghome:
+            cipher = gnupg.GPG(gnupghome=self.gnupghome, keyring=self.keyring)
         else:
-            return gnupg.GPG()
-
-
-    def encrypt(self, data, recipients):
-        crypted = self.encrypt(
+            cipher = gnupg.GPG()
+        crypted = cipher.encrypt(
             data,
-            recipients = recipients,
+            recipients = self.recipients,
             always_trust = True
         )
-        return crypted
+        if crypted.ok:
+            return crypted.data.decode()
+        else:
+            return f"encription error with status: {crypted.status}"
     
     def decrypt(self, data, passphrase=None):
+        if self.gnupghome:
+            cipher = gnupg.GPG(gnupghome=self.gnupghome, keyring=self.keyring)
+        else:
+            cipher = gnupg.GPG()
         if passphrase:
-            clear = self.decrypt(
+            clear = cipher.decrypt(
                 data,
                 passphrase = passphrase
             )
         else:
-            clear = self.decrypt(
+            clear = cipher.decrypt(
                 data,
             )
-        return clear
+        if clear.ok:
+            return clear.data.decode()
+        else:
+            return f"encription error with status: {clear.status}"
     
-def EncryptPassword(data, cipher, recipients=None):
+def EncryptPassword(data):
     '''
     Encrypt the given data/string of password with cipher
     '''
-    return cipher.encrypt(data, recipients)
+    cipher = GPGCipher(GNUPGHOME, KEYRING, RECIPIENTS)
+    encoded = cipher.encrypt(data)
+    print(f"Created object cipher of type: {type(cipher)}")
+    print(f"password: {data} encoded as:\n{encoded}")
+    print(f"type of encoded pwd: {type(encoded)}")
+    return encoded
 
-def DecryptPassword(data, cipher, passphrase):
+def DecryptPassword(data):
     '''
     Decrypt the given data/string of encoded password with cipher.
     '''
-    return cipher.decrypt(data, passphrase)
+    cipher = GPGCipher(GNUPGHOME, KEYRING, RECIPIENTS)
+    clear = cipher.decrypt(data)
+    print(f"encrypted password:\n{data}")
+    print(f"decoded as: {clear}")
+    return clear
